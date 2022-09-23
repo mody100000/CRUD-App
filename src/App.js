14658ddbe78f49ba4b7721d296df0cca6d6f8931
mody100000 <m.anamody10000@gmail.com>
2022-09-23 13:26:47 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from "react";
-import UserTabel from "./tabels/UserTabel";
+import React, { useState } from "react";
+import UserTable from "./tables/UserTable";
+import AddUserForm from "./forms/AddUserForm";
 const App = () => {
+  const usersData = [
+    { id: 1, name: "mody", username: "mody10000" },
+    { id: 2, name: "mosh", username: "moshhamdani" },
+    { id: 3, name: "whatever", username: "whatever1000" },
+  ];
+
+  const [users, setUsers] = useState(usersData);
+
+  const addUser = (user) => {
+    user.id = users.length + 1;
+    setUsers([...users, user]);
+  };
   return (
     <div className="container">
       <h1>CRUD App Using Hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
           <h2>Add user</h2>
+          <AddUserForm addUser={addUser} />
         </div>
         <div className="flex-large">
           <h2>View users</h2>
-          <UserTabel />
+          <UserTable users={users} />
         </div>
       </div>
     </div>
