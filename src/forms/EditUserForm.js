1f44cc1187215524f58b09543f8caab68b331648
mody100000 <m.anamody10000@gmail.com>
2022-09-23 22:17:41 +0200

    final step

--- src/forms/EditUserForm.js
+++ src/forms/EditUserForm.js
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 const EditUserForm = (props) => {
   const [user, setUser] = useState(props.currentUser);
+
+  useEffect(() => {
+    setUser(props.currentUser);
+  }, [props]);
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-
     setUser({ ...user, [name]: value });
   };
   return (
